45ce54260ed5a5a2759a6794eb649e6fa55a374f
Jake Seunghyun Cho <78609676+SeunghyunCho22@users.noreply.github.com>
2024-01-23 23:49:02 +0900

    Title: 1부터의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240123/1부터의 합/sum-from-the-one.c
+++ 240123/1부터의 합/sum-from-the-one.c
@@ -4,13 +4,15 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     scanf("%d", &n);
+    int k;
     int sum=0;
     for (int i=1; i<=100; i++){
         sum+=i;
+        k=i;
         if (sum>=n){
             break;
         }
     }
-    printf("%d", i);
+    printf("%d", k);
     return 0;
 }
