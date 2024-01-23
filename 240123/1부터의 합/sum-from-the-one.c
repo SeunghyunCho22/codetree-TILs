f38e73bd116f2aa857a0508329e23dcae719ddf5
Jake Seunghyun Cho <78609676+SeunghyunCho22@users.noreply.github.com>
2024-01-23 23:48:20 +0900

    Title: 1부터의 합, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240123/1부터의 합/sum-from-the-one.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d", &n);
+    int sum=0;
+    for (int i=1; i<=100; i++){
+        sum+=i;
+        if (sum>=n){
+            break;
+        }
+    }
+    printf("%d", i);
+    return 0;
+}
